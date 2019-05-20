8627b96899b174645e1e51b1b462942b82a67287
afa4 <afa4@cin.ufpe.br>
2019-05-20 16:56:53 -0300

    Creating radio input component

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,12 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model';
+import {RadioOption} from '../shared/radio/radio-option.model';
+import {visitValue} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    new RadioOption('Dinheiro', 'MON'),
+    new RadioOption('Crédito', 'CRE'),
+    new RadioOption('Débito', 'DEB'),
+  ];
 
   constructor(public shoppingCartService: ShoppingCartService) {
   }
